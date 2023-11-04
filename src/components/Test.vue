38fe68ee42e1ad35c4efec11af9aef79f3b66d71
Martin <martbelmoaw@gmail.com>
2023-11-04 15:12:35 +0300

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ text }}</p>
+    <p>{{ textComputed }}</p>
+    <button @click="clickHandler">Нажми</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, watch } from "vue";
+
+const text = ref("Тестовый текст");
+
+const textComputed = computed(() => {
+  if (text.value === "Тестовый текст") return "Ага";
+  return "Неа";
+});
+
+function clickHandler() {
+  text.value = "Нажата кнопка";
+}
+
+watch(text, () => {
+  console.log("Текст поменялся");
+});
+</script>
+
+<style scoped>
+p {
+  color: white;
+}
+</style>
